bd3f7a43a3ed20ee9969e12217d7e242f4b0a2c3
soliCat1 <vovachegotov@gmail.com>
2024-09-08 20:24:32 +0300

    Добавляет адаптив

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-var swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
+var swiper = new Swiper(".swiper", {
+  direction: "horizontal",
   slidesPerView: 4,
   spaceBetween: 10,
   loop: false,
   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-      320: {
-          slidesPerView: 1,
-          spaceBetween: 20
-      },
-      375: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-      },
-      768: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-      },
-      1024: {
-          slidesPerView: 3,
-          spaceBetween: 40,
-      },
-      1280: {
-          slidesPerView: 4,
-          spaceBetween: 24
-      }
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
   },
 });
